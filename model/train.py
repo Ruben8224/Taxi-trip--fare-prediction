112fa3b2be0fc9fc056f58167a0677d152be9ffb
Ruben8224 <Tartas_casr@hotmail.com>
2023-10-31 23:41:40 -0600

    master

--- model/train.py
+++ model/train.py
@@ -7,21 +7,25 @@ import pathlib
 # Load the dataset
 df = pd.read_csv(pathlib.Path('data/train.csv'))
 
-# Define 'y' as the target variable (fare)
-y = df['fare']
+try:
+    # Define 'y' as the target variable (fare)
+    y = df['fare']
 
-# Keep only the relevant features for training
-selected_features = ['trip_duration', 'distance_traveled', 'num_of_passengers', 'miscellaneous_fees']
-X = df[selected_features]
+    # Keep only the relevant features for training
+    selected_features = ['trip_duration', 'distance_traveled', 'num_of_passengers', 'miscellaneous_fees']
+    X = df[selected_features]
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    # Split the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Create a RandomForestRegressor model
-print('Training model.. ')  #15, 6
-regressor = RandomForestRegressor(n_estimators=15, max_depth=6, random_state=0)  # Adjust the values here
-regressor.fit(X_train, y_train)
+    # Create a RandomForestRegressor model
+    print('Training model..')
+    regressor = RandomForestRegressor(n_estimators=15, max_depth=6, random_state=0)  # Adjust the values here
+    regressor.fit(X_train, y_train)
 
-# Save the trained model
-print('Saving model..')
-dump(regressor, pathlib.Path('model/train-regression-v1.joblib'))
+    # Save the trained model
+    print('Saving model..')
+    dump(regressor, pathlib.Path('model/train-regression-v1.joblib'))
+
+except KeyError as e:
+    print(f"Error: The 'fare' column is not found in the dataset. Please check your CSV file and column names.")
