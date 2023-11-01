0f1ab78f30f686b65ff7e3c9073b7cf354993fe1
Ruben8224 <Tartas_casr@hotmail.com>
2023-10-31 23:02:21 -0600

    master

--- model/train.py
+++ model/train.py
@@ -7,19 +7,19 @@ import pathlib
 # Load the dataset
 df = pd.read_csv(pathlib.Path('data/train.csv'))
 
-# Define 'y' as the target variable (Annual_Premium)
-y = df['Annual_Premium']
+# Define 'y' as the target variable (fare)
+y = df['fare']
 
 # Keep only the relevant features for training
-selected_features = ['Age', 'Driving_License', 'Region_Code', 'Previously_Insured', 'Policy_Sales_Channel', 'Vintage']
+selected_features = ['trip_duration', 'distance_traveled', 'num_of_passengers', 'miscellaneous_fees']
 X = df[selected_features]
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Create a RandomForestRegressor model
-print('Training model.. ')
-regressor = RandomForestRegressor(n_estimators=40, max_depth=15, random_state=0)  # Adjust the values here
+print('Training model.. ')  #15, 6
+regressor = RandomForestRegressor(n_estimators=15, max_depth=6, random_state=0)  # Adjust the values here
 regressor.fit(X_train, y_train)
 
 # Save the trained model
